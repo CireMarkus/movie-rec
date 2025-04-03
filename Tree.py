0ba4e9f294120bed8f419a38f5ea00f96b6460fe
CireMaruks <mrkimjarrow95@gmail.com>
2025-04-02 20:40:40 -0500

    moved the tree traversal algorithm into it's own function. Updated the add_node function to handle an empty tree. Added in order and reverse order traversal. added some testing data.

--- Tree.py
+++ Tree.py
@@ -1,4 +1,5 @@
 from Node import Node
+from random import randint
 class Tree: 
     def __init__(self,root = None):
         if type(root) == Node:
@@ -6,12 +7,22 @@ class Tree:
         else:
             self.root = Node(root)
     
-   
+    def tree_trav(self,current_node,needtovisit):
+        #function to traverse the tree
+        if current_node.getLeft() or current_node.getRight():
+            if current_node.getLeft():
+                needtovisit.append(current_node.getLeft())
+            if current_node.getRight():
+                needtovisit.append(current_node.getRight())
+
     
     def add_node(self,node):
         #function to add a node to the tree
         new_node =  node
         current_node = self.root
+        if self.root.data == None:
+            self.root = new_node
+            return
         if type(new_node) != Node:
             new_node = Node(new_node)
         assigned = False
@@ -30,47 +41,67 @@ class Tree:
                     assigned = True
             
     def print_tree(self):
-        #function to traverse the tree and print the tree. 
+        #function to traverse the tree and print the tree. BFS
         needtovisit = []
         needtovisit.append(self.root)
 
         while needtovisit: 
             current_node = needtovisit.pop(0)
-            if current_node.getLeft() or current_node.getRight():
-                if current_node.getLeft():
-                    needtovisit.append(current_node.getLeft())
-                if current_node.getRight():
-                    needtovisit.append(current_node.getRight())
+            self.tree_trav(current_node,needtovisit)
             print(current_node.getData())
             
     def search(self,value):
+        #function to search for a value in the tree
+        pass
+    
+    
+    
+    def inorder_trav(self,current_node):
+        #function to traverse the tree in order left, root,right
+        if current_node.getLeft():
+            self.inorder_trav(current_node.getLeft())
         
-        pass
-
-    def inorder_trav(self):
+        print(current_node.getData())
         
-        pass
-    def preorder_trav(self):
-        pass
-    def postorder_trav(self):
-        pass
+        if current_node.getRight():
+            self.inorder_trav(current_node.getRight())
+        
+            
+        
+            
+    def revorder_trav(self,current_node):
+        #function to traverse the tree in reverse order right, root,left
+        if current_node.getRight():
+            self.revorder_trav(current_node.getRight())
+        
+        print(current_node.getData())    
+        
+        if current_node.getLeft():
+            self.revorder_trav(current_node.getLeft())
+            
+        
+        
+        
+        
+        
 
-node1 = Node(12)
-node2 = Node(14)
-node3 = Node(11)
-node4 = Node(55)
+# node1 = Node(12)
+# node2 = Node(14)
+# node3 = Node(11)
+# node4 = Node(55)
 
 
 
+    
 
-tree1 = Tree(node1)
+tree1 = Tree()
 
-tree1.add_node(node2)
-tree1.add_node(node3)
-tree1.add_node(node4)
+for i in range (10):
+    tree1.add_node(Node(randint(0,100)))
+
 tree1.print_tree()
-
-
+print("Post order traversal")
+tree1.revorder_trav(tree1.root)
 
             
             
