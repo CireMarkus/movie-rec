87bfc3c4414bda7a0232dee8f28b3eacd818d6de
CireMarkus <mrkimjarrow95@gmail.com>
2025-04-09 08:23:40 -0500

    Uploading to work on another computer.

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from Movie import Movie
+from Tree import Tree
 import json
+
+genre_dict = {}
+Movies = Tree()
+
+def menu():
+    pass
+
+def populate_genre_dict():
+    pass
+
+def load_movies():
+    with open('movies.json', 'r') as file:
+        data = json.load(file)
+    
+    pass 
+
+
+def main():
+    pass
 
