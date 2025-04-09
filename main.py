0e4b9ae769cccfe30c3584f5b9f8b1a1a96959bb
CireMarkus <mrkimjarrow95@gmail.com>
2025-04-09 14:48:58 -0500

    Added various methods that will be used in the program.

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
+import json
 from Movie import Movie
 from Tree import Tree
-import json
+
 
 genre_dict = {}
 Movies = Tree()
 
 def menu():
+    '''
+    Function to display the menu to the user.
+    '''
     pass
 
 def populate_genre_dict():
+    '''
+    Function to populate the genre dictionary with the genres of the movies.
+    '''
     pass
 
 def load_movies():
+    '''
+    Load the movies from the json file into the movies tree
+    '''
     with open('movies.json', 'r') as file:
         data = json.load(file)
-    
-    pass 
+    #Pull the data from the json file
+    #load the data into the movies tree
+    pass
+
+def save_genre_dict():
+    '''
+    Save the genre dictionary to a json file for later use. Will also create a new file if it does not exist.
+    '''
+    pass
+
+def load_genre_dict():
+    #load the genre dictionary from a json file
+    pass
 
 
 def main():
     pass
 
+if __name__ == "__main__":
+    pass
