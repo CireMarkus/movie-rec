1e6dda6e80f713b6ea7503d2faba3dad48a614af
CireMaruks <mrkimjarrow95@gmail.com>
2025-04-13 21:18:38 -0500

    Implement genre loading functionality in load_genres function

--- main.py
+++ main.py
@@ -21,12 +21,27 @@ def load_genres():
     try:
         #if there is a file load the genre dictionary from the file
         with (open('Genre.json', 'r')) as file:
-            pass
+            data = json.load(file)
+            #load the data into the genre dictionary
+            for datum in data: 
+                genre_dict[datum['genre']] = set(datum['movies'])
     except FileNotFoundError:
-    #if there is not a file traverse the movie tree 
-        #  to populate the genre dictionary with genres and movies that fit said genre
-        # add the genres to the genere dictionary as a set
-        pass
+        movie_list = Movies.inorder_trav(Movies.root)
+        #check to see if the movie tree is empty
+        if not movie_list:
+            print("The movie tree is empty.")
+            return
+        #traverse the movie tree and add the genres to the genre dictionary
+        # add movie to each of the generes that it fits
+        for movie in movie_list:
+            for genre in movie.getGenre():
+                if genre not in genre_dict:
+                    genre_dict[genre] = set()
+                genre_dict[genre].add(movie.getTitle())
+    except Exception as e:
+        #catch any other exceptions and print the error message
+        print(f"unexpected error: {e}, type: {type(e)}")
+        raise e
         
 
     
@@ -45,13 +60,13 @@ def load_movies(movie_tree):
         for datum in data:
             movie = Movie(datum['title'], datum['director'])
             
-            for i in datum['actors']:
-                movie.addActor(i)
+            for actor in datum['actors']:
+                movie.addActor(actor)
                 
             movie.setRTRating(datum['RTR'])
             
-            for i in datum['genre']:
-                movie.setGenre(i)
+            for genre in datum['genre']:
+                movie.setGenre(genre)
                 
             node = Node(movie)
             movie_tree.add_node(node)
@@ -80,5 +95,8 @@ if __name__ == "__main__":
     movie_list = Movies.inorder_trav(Movies.root)
     for i in movie_list:
         print(i.getTitle(),i.getGenre())
+    
+    load_genres()
+    print(genre_dict)
 
     
