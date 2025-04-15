fb89c98f6bcee91293d68612a684764faf84d585
CireMarkus <mrkimjarrow95@gmail.com>
2025-04-14 20:25:00 -0500

    Fix director initialization and enhance movie serialization methods

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def load_genres():
             data = json.load(file)
             #load the data into the genre dictionary
             for datum in data: 
-                genre_dict[datum['genre']] = set(datum['movies'])
+                genre_dict[datum] = set(data[datum])
     except FileNotFoundError:
         movie_list = Movies.inorder_trav(Movies.root)
         #check to see if the movie tree is empty
@@ -58,13 +58,24 @@ def load_movies(movie_tree):
     
         #load the data into the movies tree
         for datum in data:
-            movie = Movie(datum['title'], datum['director'])
+            movie = Movie(datum['title'])
             
+            if type(datum['director']) == str:
+                movie.addDirector(datum['director'])
+            else:
+                for director in datum['director']:
+                    movie.addDirector(director)
+
             for actor in datum['actors']:
                 movie.addActor(actor)
                 
             movie.setRTRating(datum['RTR'])
             
+            try:
+                movie.setUserRating(datum['userRating'])
+            except KeyError:
+                movie.setUserRating(None)
+
             for genre in datum['genre']:
                 movie.setGenre(genre)
                 
@@ -76,14 +87,28 @@ def save_genre_dict():
     Save the genre dictionary to a json file for later use. 
     Will also create a new file if it does not exist.
     '''  
-    #try dumping the genre dictionary to a json file? 
+    for i in genre_dict:
+        #convert the set to a list
+        genre_dict[i] = list(genre_dict[i])
+    with open('Genre.json', 'w') as file:
+        #create a list of dictionaries to save the genre dictionary
+        #dump the data to the json file
+        json.dump(genre_dict, file, indent=4)
 
 def save_movies():
     '''
     save the movies to a json file for later use.
     Will also create a new file if it does not exist.
     '''
-    #try dumping the movie tree to a json file?
+    with open('Movies.json', 'w') as file:
+        #create a list of dictionaries to save the movies
+        movie_list = Movies.inorder_trav(Movies.root)
+        data = []
+        for movie in movie_list:
+            data.append(movie.to_dict())
+        #dump the data to the json file
+        json.dump(data, file, indent=4)
+        #create a list of dictionaries to save the movies
     
     
 def main():
@@ -95,8 +120,11 @@ if __name__ == "__main__":
     movie_list = Movies.inorder_trav(Movies.root)
     for i in movie_list:
         print(i.getTitle(),i.getGenre())
+        i.addDirector("Eric Washington")
     
     load_genres()
     print(genre_dict)
 
+    save_genre_dict()
+    save_movies()
     
