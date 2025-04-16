80ab5e5ad2caea0d4eb1a8902b6cb7d8e2cc25df
CireMaruks <mrkimjarrow95@gmail.com>
2025-04-16 00:58:41 -0500

    Fix director initialization and enhance genre handling in Movie class

--- Movie.py
+++ Movie.py
@@ -6,7 +6,7 @@ class Movie:
         self.rtRating = None
         self.userRating = None
         self.genres = []
-        self.directors.append(director)
+        self.directors.append(director.split(','))
         
     def setTitle(self,title):
         self.title = title
@@ -22,9 +22,9 @@ class Movie:
         
     def addDirector(self, director):
         if None not in self.directors:
-            self.directors.append(director)
+            self.directors.append(director.split(','))
         else: 
-            self.directors = [director]
+            self.directors = director.split(',')
             
     def getDirectors(self):
         return self.directors
@@ -42,7 +42,16 @@ class Movie:
         return self.userRating
         
     def setGenre(self,genre):
-        self.genres.append(genre)
+        if isinstance(genre, list):
+            # Check if genre is a list
+            # If it's not already in the list, append it
+            for g in genre:
+                if g not in self.genres:
+                    self.genres.append(g)
+        elif genre not in self.genres:
+            # Check if genre is a string
+            # If it's not already in the list, append it
+            self.genres.append(genre)
         
     def getGenre(self):
         return self.genres
